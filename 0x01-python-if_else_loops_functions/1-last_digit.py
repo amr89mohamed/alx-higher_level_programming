dd0018b4f425bb41dd55151e73d9d41213f8da9d
amr89mohamed <amr89mohamed@gmail.com>
2024-03-12 16:52:15 -0700

    handel the nagtive case for the last_digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,14 @@ if number < 0:
 else:
     the_number = number
 last_digit = the_number % 10
-if last_digit > 5:
+if number < 0:
+    the_last_digit = last_digit * -1
+else:
+    the_last_digit = last_digit
+if the_last_digit > 5:
     print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
-elif last_digit < 6 and last_digit != 0:
+elif the_last_digit < 6 and last_digit != 0:
     print(f"Last digit of {number:d} is {last_digit:d} \
 and is less than 6 and not 0")
-elif last_digit == 0:
+elif the_last_digit == 0:
     print(f"Last digit of {number:d} is {last_digit:d} and is 0")
