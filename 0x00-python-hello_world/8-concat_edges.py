ca9ab91535789f7527deec097a8fa07825046ebd
amr89mohamed <amr89mohamed@gmail.com>
2025-02-25 03:50:15 -0800

    run the pycodestyle to the 8-concat_edges file and all is good

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
   language that combines remarkable power with very clear syntax"
-str = str.split( )
-print( " ".join( str[5:7] + [str[12] + " " + str[0]] ) )
+str = str.split()
+print(" ".join(str[5:7] + [str[12] + " " + str[0]]))
