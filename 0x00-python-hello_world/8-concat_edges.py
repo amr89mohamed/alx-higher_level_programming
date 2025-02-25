a0601c025752f51f55d787ef215377d00baf7329
amr89mohamed <amr89mohamed@gmail.com>
2025-02-25 03:17:54 -0800

    make the 8-concat_edges file less than 6 lines

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
 list = str.split( )
 words =  " ".join(list[5:7] + [list[12] + " " + list[0]])
 print(words)
