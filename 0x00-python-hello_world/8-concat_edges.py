10557b5f11f71f45aed25f241cf91687bcdae3c6
amr89mohamed <amr89mohamed@gmail.com>
2025-02-25 03:22:41 -0800

    only str var is in the 8-concat edges file

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
-list = str.split( )
-words =  " ".join(list[5:7] + [list[12] + " " + list[0]])
-print(words)
+str = "Python is an interpreted, interactive, object-oriented programming \language that combines remarkable power with very clear syntax"
+str = str.split( )
+str =  " ".join(str[5:7] + [str[12] + " " + str[0]])
+print(str)
