be1e4df8dada79e2d097bed53eb041c90f5e65cc
amr89mohamed <amr89mohamed@gmail.com>
2025-02-25 03:45:04 -0800

    all is spaced in the 8-concat file

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+  language that combines remarkable power with very clear syntax"
 str = str.split( )
-print(" ".join(str[5:7] + [str[12] + " " + str[0]]))
+print( " ".join( str[5:7] + [str[12] + " " + str[0]] ) )
